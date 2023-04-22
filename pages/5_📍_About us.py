856d533822d9c9155adf31ae7bd280f1dda6d188
Abhishek Dwibedy <65005291+abhishekdwibedy-2002@users.noreply.github.com>
2023-04-22 17:49:15 +0530

    Update 5_📍_About us.py

--- pages/5_📍_About us.py
+++ pages/5_📍_About us.py
@@ -28,7 +28,7 @@ st.sidebar.image(image , use_column_width=True, output_format='auto')
 st.sidebar.markdown("---")
 
 
-st.sidebar.markdown("<br> <br> <br> <br> <br> <br> <br> <h1 style='text-align: center; font-size: 18px; color: #0080FF;'>© 2023 | Ioannis Bakomichalis</h1>", unsafe_allow_html=True)
+st.sidebar.markdown("<br> <br> <br> <br> <br> <br> <br> <h1 style='text-align: center; font-size: 18px; color: #0080FF;'>© 2023 </h1>", unsafe_allow_html=True)
 
 
 
@@ -37,16 +37,16 @@ st.title("About us📍")
 st.markdown("---")
 st.markdown("<br>", unsafe_allow_html=True)
 
-st.markdown("The project **_CBDA_** developed by Postgraduate student :blue[**_Ioannis Bakomichalis_**] for his MSc Dissertation :blue[**_Cyberbullying Detection through NLP & Machine Learning_**].")
+st.markdown("The project **_CBDA_** developed by Undergraduate student :blue[**_Group 1_**] for his B.Tech Final Year Major Project :blue[**_Cyberbullying Detection through Machine Learning_**].")
 st.markdown(" <br> ", unsafe_allow_html=True)
 st.markdown("<br>", unsafe_allow_html=True)
-st.markdown("For any question about the project, please contact <a href=mailto:ioannis.bakomichalis@ssl-unipi.gr>Ioannis Bakomichalis</a> .", unsafe_allow_html=True)
+st.markdown("For any question about the project, please contact <a href=mailto:dwibedyabhishek1@gmail.com>Group -1</a> .", unsafe_allow_html=True)
 st.markdown("<br> <br> <br>", unsafe_allow_html=True)
 col1, col2 = st.columns([2,4])
 with col1:
-    image = Image.open('icons/unipiLogo.png')
+    image = Image.open('icons/logo.jpeg')
     st.image(image, use_column_width=False, output_format='auto')
 with col2:
     st.markdown("<br> ", unsafe_allow_html=True)
-    st.markdown("<h1 style='text-align: center; font-size: 15px; color: #002d51;'>University of Piraeus, Department of Digital Systems</h1>", unsafe_allow_html=True)
-    st.markdown("<h1 style='text-align: center; font-size: 15px; color: #002d51;'>M.Sc. Digital Systems Security</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; font-size: 15px; color: #002d51;'>Odisha University of Technology And Research,Bhubaneswar, Odisha</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; font-size: 15px; color: #002d51;'>B.TECH Information Technology</h1>", unsafe_allow_html=True)
